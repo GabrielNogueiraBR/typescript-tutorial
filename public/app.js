2511cbb1fa2b4bda9ab08dbbfea97f1057a92c16
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-25 16:31:05 -0300

    feat: Modules in TS

--- public/app.js
+++ public/app.js
@@ -1,14 +1,3 @@
-"use strict";
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    ;
-    format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice";
 const invOne = new Invoice('Gabriel', 'work on R2U', 999999999);
 const invTwo = new Invoice('João', 'work on Amazon', 10000);
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,4 @@
-class Invoice {
-
-    constructor(
-        readonly client: string,
-        private details: string,
-        public amount: number
-    ){};
-
-    format(){
-        return `${this.client} owes ${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/Invoice";
 
 const invOne = new Invoice('Gabriel','work on R2U',999999999);
 const invTwo = new Invoice('João','work on Amazon',10000);

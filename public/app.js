b07313b4edc9a373f8d285a4b385e82e58d24f6c
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-25 16:45:01 -0300

    feat: Interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,18 @@
+const me = {
+    name: 'shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log(`Hello ${person.name}`);
+};
+greetPerson(me);
 import { Invoice } from "./classes/Invoice";
 const invOne = new Invoice('Gabriel', 'work on R2U', 999999999);
 const invTwo = new Invoice('João', 'work on Amazon', 10000);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,30 @@
+// Interface
+interface IsPerson{
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: IsPerson = {
+    name: 'shaun',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log('I spent', amount);
+        return amount;
+    },
+};
+
+const greetPerson = (person: IsPerson)=>{
+    console.log(`Hello ${person.name}`);
+}
+
+greetPerson(me);
+
+
 import { Invoice } from "./classes/Invoice";
 
 const invOne = new Invoice('Gabriel','work on R2U',999999999);

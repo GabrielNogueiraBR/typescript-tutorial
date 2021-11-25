b56be54629f24a111a137aa603c59cd953d272b1
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2021-11-25 16:21:02 -0300

    feat: Public, Private & Readonly

--- public/app.js
+++ public/app.js
@@ -1,18 +1,14 @@
 "use strict";
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
+    ;
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`;
     }
 }
 const invOne = new Invoice('Gabriel', 'work on R2U', 999999999);
 const invTwo = new Invoice('João', 'work on Amazon', 10000);
-// Just Invoice Objects
-let invoices = [];
-// invoices.push('hello');
-invoices.push(invOne);
-invoices.push(invTwo);
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){};
 
     format(){
         return `${this.client} owes ${this.amount} for ${this.details}`;
@@ -16,9 +13,3 @@ class Invoice {
 
 const invOne = new Invoice('Gabriel','work on R2U',999999999);
 const invTwo = new Invoice('João','work on Amazon',10000);
-
-// Just Invoice Objects
-let invoices: Invoice[] = [];
-// invoices.push('hello');
-invoices.push(invOne);
-invoices.push(invTwo);
